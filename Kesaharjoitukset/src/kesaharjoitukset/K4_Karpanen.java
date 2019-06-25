8304e908389ddf3e5f750571420c829c85171d61
jarikol <jarikolehmainen@hotmail.com>
2019-06-25 14:03:31 +0300

    k4

---
+++ Kesaharjoitukset/src/kesaharjoitukset/K4_Karpanen.java
@@ -0,0 +1,24 @@
+package kesaharjoitukset;
+
+public class K4_Karpanen {
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+class Karpanen {
+	public void suhise() {
+		System.out.println("Kärpänen suhisee siivillään.");
+	}
+	
+	public void lenna() {
+		System.out.println("Kärpänen lentää");
+	}
+	
+	public void syo(String syote) {
+		System.out.println("Karpanen syo "+syote);
+		
+	}
+}
+
